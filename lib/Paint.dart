b36483a17f243c4f84d8de1ae0ec57a42b4c0753
heh134 <yzqwzq@qq.com>
2019-05-06 23:32:56 +0300

    add number

--- lib/Paint.dart
+++ lib/Paint.dart
@@ -12,10 +12,9 @@ final Map<int, Color> BoxColors = <int, Color>{
   256: Colors.orange[700],
   512: Colors.orange[800],
   1024: Colors.orange[900],
-  2048:Colors.red,
-  4096:Colors.red[700],
-  8192:Colors.red[800],
-
+  2048: Colors.red,
+  4096: Colors.red[700],
+  8192: Colors.red[800],
 };
 
 class Painter extends CustomPainter {
@@ -36,17 +35,38 @@ class Painter extends CustomPainter {
     final pos_Y = height / rows / 2;
     Paint paint = Paint();
     paint.strokeWidth = 1;
+    TextSpan span;
+    TextPainter tp;
+    String nums;
     for (int i = 0; i < rows; i += 1) {
       for (int j = 0; j < columns; j += 1) {
         int num = cells[i][j];
+        nums = num.toString();
         print(num);
         paint.color = BoxColors.containsKey(num)
             ? BoxColors[num]
             : BoxColors[BoxColors.keys.last];
-        if (num > 0)
+        if (num > 0) {
           canvas.drawCircle(
               Offset(pos_X + pos_X * 2 * i, pos_Y + pos_Y * 2 * j), 30, paint);
-
+          span = new TextSpan(
+              style: new TextStyle(
+                color: Colors.black54,
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+              text: nums);
+          tp = new TextPainter(
+              text: span,
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.rtl);
+          tp.layout();
+          tp.paint(
+            canvas,
+            new Offset(pos_X - 20 * nums.length * 0.3 + pos_X * 2 * i,
+                pos_Y - 14 + pos_Y * 2 * j),
+          );
+        }
       }
     }
   }
